4a4929c56328549f748f6fe5373195d7b9298928
pallavi <pallavi.hd@reconnectenergy.com>
2024-08-05 16:47:58 +0530

    locators

--- src/test/java/swd/locators.java
+++ src/test/java/swd/locators.java
@@ -1,5 +1,7 @@
 package swd;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -24,6 +26,20 @@ public class locators
 	    
 	    //link text
 	    driver.findElement(By.linkText("Tablets")).click();
+	    
+	    //classname
+	    
+	    List<WebElement> headerLinks=driver.findElements(By.className("list-inline-item"));
+	    System.out.println(headerLinks.size());
+	    
+	    //tagname
+	    
+	    List<WebElement> links=driver.findElements(By.tagName("a"));
+	    System.out.println(links.size());
+	    
+	    List<WebElement> images=driver.findElements(By.tagName("img"));
+	    System.out.println(images.size());
+	    
 
 	}
 
